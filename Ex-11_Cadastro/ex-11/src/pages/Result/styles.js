ebb391fabf3c1fd80b0ba5636b5166de9904bd59
YMark07 <85963090+YMark07@users.noreply.github.com>
2023-04-28 17:20:09 -0300

    Add files via upload

---
+++ Ex-11_Cadastro/ex-11/src/pages/Result/styles.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  },
+    texto:{
+    fontSize: 22,
+  },
+  titulo:{
+  textAlign: 'center',
+  fontSize: 25,
+  margin: 35
+  
+  },
+  pressable:{
+    width: 250,
+    height: 60,
+    margin: 35,
+    backgroundColor: 'green',
+  }
+},
+)
+
+export {styles}
