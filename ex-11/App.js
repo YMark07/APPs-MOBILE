137a405e1166b0b4c828a65ea2282df3794de417
YMark07 <85963090+YMark07@users.noreply.github.com>
2023-04-28 17:19:17 -0300

    Add files via upload

---
+++ ex-11/App.js
@@ -0,0 +1,24 @@
+App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import Home from './src/pages/Home';
+import Result from './src/pages/Result';
+
+
+const Stack = createStackNavigator();
+
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+        <Stack.Screen name="Result" component={Result} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
