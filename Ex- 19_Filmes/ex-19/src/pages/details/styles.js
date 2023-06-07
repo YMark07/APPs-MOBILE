cf9a5d723f8e77e83e650a5e233b6a170ff8ed05
YMark07 <85963090+YMark07@users.noreply.github.com>
2023-06-07 18:03:41 -0300

    Add files via upload

---
+++ Ex- 19_Filmes/ex-19/src/pages/details/styles.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  titulo:{
+    marginTop: 5,
+    fontSize: 18,
+    textAlign: 'center'
+  },
+  texto:{
+    fontSize: 16,
+  },
+})
+
+export {styles};
---
+++ Ex- 19_Filmes/ex-19/src/pages/film/styles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+   container:{
+    flex: 1,
+    marginTop: 60,
+    alignItems: 'center'
+  },
+  titulo:{
+    marginTop: 5,
+    fontSize: 18,
+    textAlign: 'center'
+  },
+  foto:{
+     width: 250, 
+     height: 100, 
+  },
+})
+
+export {styles};
